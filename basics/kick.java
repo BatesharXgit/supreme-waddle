66358c79740910264dbbd8b16e882a5f69bd1734
BatesharXgit <bateshar.xyz@gmail.com>
2024-03-18 16:18:36 +0530

    just started

---
+++ basics/kick.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class KickStart {
+     public static void main(String args[]){
+         Scanner prompt = new Scanner(System.in);
+         System.out.println("I am back");
+         int in = prompt.nextInt();
+         System.out.println(in);
+     }
+ }

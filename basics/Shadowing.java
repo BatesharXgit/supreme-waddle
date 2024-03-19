379693657cfecc88b040f6bf9e12927dcc77c199
BatesharXgit <bateshar.xyz@gmail.com>
2024-03-19 08:33:26 +0530

    someone shadows

---
+++ basics/Shadowing.java
@@ -0,0 +1,14 @@
+public class Shadowing {
+    int x = 10;
+    public void shadowingExample() {
+        int x = 20;
+        System.out.println(x);
+    }
+
+    public static void main(String[] args) {
+        Shadowing xd = new Shadowing();
+        xd.shadowingExample();
+        System.out.println(xd.x);
+
+    }
+}
--- basics/kick.java
+++ basics/kick.java
@@ -1,10 +1,9 @@
 import java.util.*;
-
 class KickStart {
-     public static void main(String args[]){
-         Scanner prompt = new Scanner(System.in);
-         System.out.println("I am back");
-         int in = prompt.nextInt();
-         System.out.println(in);
-     }
- }
+    public static void main(String args[]){
+        Scanner prompt = new Scanner(System.in);
+        System.out.println("I am back");
+        int in = prompt.nextInt();
+        System.out.println(in);
+    }
+}

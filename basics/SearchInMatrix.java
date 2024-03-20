fde480760c4d67480f9659c2a9fbaeb182a140af
BatesharXgit <bateshar.xyz@gmail.com>
2024-03-20 18:34:42 +0530

    something found in matrix

---
+++ basics/SearchInMatrix.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class SearchInMatrix {
+    public static void main(String[] args){
+        Scanner prompt = new Scanner(System.in);
+        int number = prompt.nextInt();
+        int[][] numbers = {
+                {2,3,4,5,6},
+                {7,8,9,10,11},
+        };
+        for (int i = 0; i<2; i++){
+            for(int j =0; j <5; j++){
+                if(numbers[i][j] == number){
+                    System.out.println(i +" "+" "+ j);
+                }
+            }
+        }
+    }
+}

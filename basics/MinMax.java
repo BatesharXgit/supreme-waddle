87f3d587c8898009bfa4702670f54cd9adab3cc1
BatesharXgit <bateshar.xyz@gmail.com>
2024-03-20 14:54:16 +0530

    who's big and who's small

--- basics/MinMax.java
+++ basics/MinMax.java
@@ -1,6 +1,19 @@
 public class MinMax {
     public static void main(String[] args){
-        int[] array = {2,4,6,8,10};
-        
+        int[] numbers = {2,4,6,8,10};
+
+        int max = numbers[0];
+        int min = numbers[0];
+
+        for (int i = 0; i<numbers.length; i++){
+            if(numbers[i] > max){
+                max = numbers[i];
+            }
+            if(numbers[i] < min){
+                min = numbers[i];
+            }
+        }
+        System.out.println(max);
+        System.out.println(min);
     }
 }

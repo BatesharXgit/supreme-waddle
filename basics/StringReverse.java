6fd3ea27b413d4bdff0a3efcde6eef7518e99a4f
BatesharXgit <bateshar.xyz@gmail.com>
2024-03-22 10:53:24 +0530

    string to array

--- basics/StringReverse.java
+++ basics/StringReverse.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class StringReverse {
     public static void main(String[] args){
         StringBuilder name = new StringBuilder("Yogesh");
---
+++ basics/StringToArray.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class StringToArray {
+    public static void main(String[] args){
+        StringBuilder name = new StringBuilder("Yogesh");
+
+        String[] name1 = new String[6];
+
+        for (int i = 0; i<name.length(); i++){
+            name1[i] = String.valueOf(name.charAt(i));
+        }
+
+        System.out.println(Arrays.toString(name1));
+
+    }
+}

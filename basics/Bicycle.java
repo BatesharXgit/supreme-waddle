85fb125849ba07891d4b61a1bb96b472749fcd0a
BatesharXgit <bateshar.xyz@gmail.com>
2024-03-24 10:52:04 +0530

    I need to guess

--- basics/Bicycle.java
+++ basics/Bicycle.java
@@ -1,3 +1,4 @@
+import java.util.*;
 class Bicycle {
     int gear = 0;
     int speed = 0;
@@ -23,14 +24,19 @@ class BicycleWork{
     public static void main(String[] args){
         Bicycle bicycle1 = new Bicycle();
         Bicycle bicycle2 = new Bicycle();
+        Scanner prompt = new Scanner(System.in);
 
+        System.out.println("Select the gear for bicycle1");
+        int gear1 = prompt.nextInt();
         System.out.println("The details of the bicycle1");
-        bicycle1.changeGear(2);
+        bicycle1.changeGear(gear1);
         bicycle1.speedUp(20);
         bicycle1.showDetails();
 
+        System.out.println("Select the gear for bicycle2");
+        int gear2 = prompt.nextInt();
         System.out.println("The details of the bicycle2");
-        bicycle2.changeGear(4);
+        bicycle2.changeGear(gear2);
         bicycle2.speedUp(40);
         bicycle2.showDetails();
     }
---
+++ basics/Cars.java
@@ -0,0 +1,14 @@
+class Cars {
+    char company;
+    char model;
+    int engineNo;
+
+    void getDetails(char company, char model, int engineNo){
+        this.company = company;
+        this.model = model;
+        this.engineNo = engineNo;
+    }
+    void showDetails(){
+        System.out.println();
+    }
+}
